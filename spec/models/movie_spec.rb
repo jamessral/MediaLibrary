51a0e5a17400225b4bcb48156608dee5a3c582d2
James Sral <jamessral@gmail.com>
2016-04-10 15:01:47 -0400

    Add spec example to ensure user can see only their movies

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,7 +6,17 @@ RSpec.describe Movie, type: :model do
    movie_with = Movie.new(title: "Test")
    expect(movie_with).to be_valid
 
-   movie_without = Movie.new(title: '') 
+   movie_without = Movie.new(title: '')
    expect(movie_without).to be_invalid
+  end
+
+  it "is available to only its user" do
+    user1 = User.new(email: "test@test.com", id: 1)
+    user2 = User.new(email: "test@test.com", id: 2)
+    movie1 = Movie.create!(title: "Test", user_id: 1)
+    movie2 = Movie.create!(title: "Test2", user_id: 2)
+
+    expect(user1.movies).to contain_exactly(movie1)
+    expect(user2.movies).to contain_exactly(movie2)
   end
 end
