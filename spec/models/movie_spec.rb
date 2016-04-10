f114a61e755f23f37a53e7a04ab36f4738d2d1c9
James Sral <jamessral@gmail.com>
2016-04-09 21:16:59 -0400

    Add spec example for movie title validation

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "requires a movie title" do
+   movie_with = Movie.new(title: "Test")
+   expect(movie_with).to be_valid
+
+   movie_without = Movie.new(title: '') 
+   expect(movie_without).to be_invalid
+  end
 end
